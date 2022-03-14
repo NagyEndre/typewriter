d5bde5c1bd80af2fc85e6a1cd4b28abe552d9f1e
NagyEndre <endre.tamas.nagy@gmail.com>
2022-03-14 17:04:53 +0100

    FEAT(Character): Add component to display character

---
+++ src/components/Character.vue
@@ -0,0 +1,32 @@
+<template>
+  <span :style="characterStyleClasses">{{ character }}</span>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { CharacterStatus } from "../utils/types";
+
+@Component({
+  components: {},
+})
+export default class Character extends Vue {
+  @Prop(String)
+  readonly character!: string;
+
+  @Prop(Number)
+  readonly status!: number;
+
+  correctStyle = { color: "white", backgroundColor: "green" };
+  wrongStyle = { color: "white", backgroundColor: "indianred" };
+
+  get characterStyleClasses() {
+    const classes = [];
+    if (this.status === CharacterStatus.Correct) {
+      classes.push(this.correctStyle);
+    } else if (this.status === CharacterStatus.Wrong) {
+      classes.push(this.wrongStyle);
+    }
+    return classes;
+  }
+}
+</script>
