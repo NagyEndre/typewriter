bf40527a1ed29987330b10a8e5643e2c42dfec06
NagyEndre <endre.tamas.nagy@gmail.com>
2022-09-04 12:17:36 +0200

    FEAT(types): Add singleton example

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -11,7 +11,13 @@ export enum ExerciseType {
   CodeSnippet,
 }
 
+const singleton =
+  "class Singleton {\n  private static instance: Singleton;\n\n  private constructor() {}\n\n  public static getInstance(): Singleton {\n    if (Singleton.instance === undefined) {\n      Singleton.instance = new Singleton();\n    }\n    return Singleton.instance;\n  }\n}";
+const strategy =
+  'interface StorageStrategy {\n    store(data: string): void;\n}\n\nclass CloudStorageStrategy implements StorageStrategy {\n    public store(data: string) {\n        console.log("Uploading data to cloud.");\n    }\n}';
+
 export const codeSnippets = new LinkedList(
-  'export class CloudStorageStrategy implements StorageStrategy {\n    public store(data: string) {\n        console.log("Uploading data to cloud.");\n    }\n}',
-  "export class CloudStorageStrategy implements StorageStrategy {\n}"
+  strategy,
+  singleton,
+  "class CloudStorageStrategy implements StorageStrategy {\n}"
 );
