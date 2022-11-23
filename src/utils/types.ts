26459fe89589ebeb84618df1a2f34bac8e235325
NagyEndre <endre.tamas.nagy@gmail.com>
2022-11-23 19:22:23 +0100

    FEAT(types): Add template method snippet

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -15,9 +15,11 @@ const singleton =
   "class Singleton {\n  private static instance: Singleton;\n\n  private constructor() {}\n\n  public static getInstance(): Singleton {\n    if (Singleton.instance === undefined) {\n      Singleton.instance = new Singleton();\n    }\n    return Singleton.instance;\n  }\n}";
 const strategy =
   'interface StorageStrategy {\n    store(data: string): void;\n}\n\nclass CloudStorageStrategy implements StorageStrategy {\n    public store(data: string) {\n        console.log("Uploading data to cloud.");\n    }\n}';
+const template_method =
+  'abstract class TestSequence {\n  public execute() {\n    this.prepareEnvironment();\n    this.userPreparation();\n    this.run();\n    this.userCleanUp();\n    this.collectResults();\n  }\n\n  protected abstract prepareEnvironment(): void;\n\n  protected userPreparation(): void {\n    console.log("No user preparation is executed.");\n  }\n\n  protected abstract run(): void;\n\n  protected userCleanUp(): void {\n    console.log("No user clean up is executed");\n  }\n\n  protected abstract collectResults(): void;\n}';
 
 export const codeSnippets = new LinkedList(
   strategy,
   singleton,
-  "class CloudStorageStrategy implements StorageStrategy {\n}"
+  template_method
 );
