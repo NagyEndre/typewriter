4aa67c0e3fd9944e246f5e00fc2d31d94fff982c
NagyEndre <endre.tamas.nagy@gmail.com>
2022-11-23 19:31:07 +0100

    REFACTOR: Add point to end of sentence

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -16,7 +16,7 @@ const singleton =
 const strategy =
   'interface StorageStrategy {\n    store(data: string): void;\n}\n\nclass CloudStorageStrategy implements StorageStrategy {\n    public store(data: string) {\n        console.log("Uploading data to cloud.");\n    }\n}';
 const template_method =
-  'abstract class TestSequence {\n  public execute() {\n    this.prepareEnvironment();\n    this.userPreparation();\n    this.run();\n    this.userCleanUp();\n    this.collectResults();\n  }\n\n  protected abstract prepareEnvironment(): void;\n\n  protected userPreparation(): void {\n    console.log("No user preparation is executed.");\n  }\n\n  protected abstract run(): void;\n\n  protected userCleanUp(): void {\n    console.log("No user clean up is executed");\n  }\n\n  protected abstract collectResults(): void;\n}';
+  'abstract class TestSequence {\n  public execute() {\n    this.prepareEnvironment();\n    this.userPreparation();\n    this.run();\n    this.userCleanUp();\n    this.collectResults();\n  }\n\n  protected abstract prepareEnvironment(): void;\n\n  protected userPreparation(): void {\n    console.log("No user preparation is executed.");\n  }\n\n  protected abstract run(): void;\n\n  protected userCleanUp(): void {\n    console.log("No user clean up is executed.");\n  }\n\n  protected abstract collectResults(): void;\n}';
 
 export const codeSnippets = new LinkedList(
   strategy,
