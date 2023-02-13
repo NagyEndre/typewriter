a44132828f70afba6286d081a0178429448dbbce
NagyEndre <endre.tamas.nagy@gmail.com>
2023-02-13 16:30:34 +0100

    FEAT(consts): Upgrade strategy snippet

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -9,8 +9,8 @@ export const newLineCharacter = "\n";
 const singleton =
   "class Singleton {\n  private static instance: Singleton;\n\n  private constructor() {}\n\n  public static getInstance(): Singleton {\n    if (!Singleton.instance) {\n      Singleton.instance = new Singleton();\n    }\n    return Singleton.instance;\n  }\n}";
 const strategy =
-  'interface StorageStrategy {\n    store(data: string): void;\n}\n\nclass CloudStorageStrategy implements StorageStrategy {\n    public store(data: string) {\n        console.log("Uploading data to cloud.");\n    }\n}';
+  'interface StorageStrategy {\n    store(data: string): void;\n}\n\nclass CloudStorage implements StorageStrategy {\n    public store(data: string) {\n        console.log("Uploading data to cloud.");\n    }\n}\n\nclass LocalStorage implements StorageStrategy {\n    public store(data: string) {\n        console.log("Saving to hard drive.");\n    }\n}\n\nclass Client {\n    private strategy: StorageStrategy;\n\n    public setStrategy(s: StorageStrategy) {\n        this.strategy = s;\n    }\n\n    public save(data: string) {\n        this.strategy.store(data);\n    }\n}\n\nconst cloud = new CloudStorage();\nconst local = new LocalStorage();\nconst client = new Client();\n\nclient.setStrategy(cloud);\nclient.save("secret")\n\nclient.setStrategy(local);\nclient.save("secret")';
 const template_method =
   'abstract class TestSequence {\n  public execute() {\n    this.prepareEnvironment();\n    this.userPreparation();\n    this.run();\n    this.userCleanUp();\n    this.collectResults();\n  }\n\n  protected abstract prepareEnvironment(): void;\n\n  protected userPreparation(): void {\n    console.log("No user preparation is executed.");\n  }\n\n  protected abstract run(): void;\n\n  protected userCleanUp(): void {\n    console.log("No user clean up is executed.");\n  }\n\n  protected abstract collectResults(): void;\n}';
 
-export const codeSnippets = [strategy, singleton, template_method];
+export const codeSnippets = [strategy];
