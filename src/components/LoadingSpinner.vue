76efc53003f55532e6e6719ccf2302721226e8d0
Endre Nagy <endretamasnagy@gmail.com>
2023-05-06 10:09:00 +0200

    FEAT: Add LoadingSpinner component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="loading-spinner">
+    <div class="spinner"></div>
+    <div id="loading-text">Loading...</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LoadingSpinner",
+});
+</script>
+
+<style>
+#loading-text {
+  color: whitesmoke;
+}
+
+.loading-spinner {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+}
+
+.spinner {
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  border: 0.4rem solid whitesmoke;
+  border-top-color: green;
+  animation: spinner 0.8s ease-in-out infinite;
+  margin: 0.5rem;
+}
+
+@keyframes spinner {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
